44f1b4e1e63e311ffcb79a267db5dd2e8a4e216e
Pedro <pedrohcastrot@gmail.com>
2025-06-17 15:55:36 -0300

    Adiciona desserialização de JSON para a classe Jogo
    - Cria a classe InfoJogo para representar o objeto 'info' do JSON.
    - Utiliza @SerializedName para mapear os campos do JSON para os atributos da classe Jogo.
    - Atualiza o método main para desserializar a resposta da API corretamente.

--- src/main/kotlin/Jogo.kt
+++ src/main/kotlin/Jogo.kt
@@ -1,2 +1,14 @@
-class Jogo {
+import com.google.gson.annotations.SerializedName
+
+class Jogo (@SerializedName("title") val titulo:String, @SerializedName ("thumb") val capa:String) {
+    val descricao = ""
+
+    override fun toString(): String {
+        return "Meu Jogo \n" +
+                "Titulo: $titulo \n" +
+                "Capa: $capa \n" +
+                "Descricao: $descricao \n"
+    }
+
+
 }
