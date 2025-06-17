346bb25bf95fcb819d66b5338c898f2947b65e41
Pedro <pedrohcastrot@gmail.com>
2025-06-17 16:34:21 -0300

    Transformação das classes Jogo e InfoJogo em data classes. Remoção do @SerializedName e refatoração do código para melhor organização e clareza. Implementação de métodos toString() personalizados para facilitar a visualização das informações dos jogos.

--- src/main/kotlin/InfoJogo.kt
+++ src/main/kotlin/InfoJogo.kt
@@ -1,3 +1,4 @@
+data class InfoJogo(val info: InfoApiShark) {
     override fun toString(): String {
         return info.toString()
     }
--- src/main/kotlin/Jogo.kt
+++ src/main/kotlin/Jogo.kt
@@ -1,2 +1,10 @@
-class Jogo {
+data class Jogo(val titulo:String,
+                val capa:String) {
+    var descricao: String? = null
+    override fun toString(): String {
+        return "Meu Jogo: \n" +
+                "Título: $titulo \n" +
+                "Capa: $capa \n" +
+                "Descricao: $descricao"
+    }
 }
